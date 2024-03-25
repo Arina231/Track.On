e92bb4e1ef9c3d59e54a9926a2a3b0fe1ac4ff9f
Arina231 <arinahonaa119@gmail.com>
2024-03-25 23:24:51 +0600

    new workout

--- script.js
+++ script.js
@@ -11,6 +11,22 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+class Workout {
+  date = new DAte();
+  id = (new Date() + '').slice(-10);
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance;
+    this.duration = duration;
+  }
+}
+
+class Running extends Workout {
+  constructor(coords,distance,duration,cadence){
+     
+  }
+}
+class Cycling extends Workout {}
 class App {
   #map;
 
