6da2387ea8e1b4ef041998cd983f40c6e34b8b6f
Arina231 <arinahonaa119@gmail.com>
2024-03-28 00:55:31 +0600

    id and date

--- script.js
+++ script.js
@@ -12,21 +12,42 @@ const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 ///////
 class Workout {
-  date = new DAte();
-  id = (new Date() + '').slice(-10);
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
   constructor(coords, distance, duration) {
-    this.coords = coords;
+    this.coords = coords; //[arry of lat,lng]
     this.distance = distance;
     this.duration = duration;
   }
 }
 
 class Running extends Workout {
-  constructor(coords,distance,duration,cadence){
-     
+  constructor(coords, distance, duration, cadence) {
+    super(coords, distance, duration);
+    this.cadence = cadence;
+    this.calcPace();
+  }
+  calcPace() {
+    this.pace = this.duration / this.distance;
+    return this.pace;
   }
 }
-class Cycling extends Workout {}
+class Cycling extends Workout {
+  constructor(coords, distance, duration, elevationGain) {
+    super(coords, distance, duration);
+    this.elevationGain = elevationGain;
+    this.clacSpeed;
+  }
+  clacSpeed() {
+    this.speed = this.distance / (this.duration / 60);
+    return this.speed;
+  }
+}
+// const run1 = new Running([39, -12], 5.2, 24, 178);
+// const cycling1 = new Cycling([39, -12], 27, 95, 523);
+// console.log(run1, cycling1);
+
+//////////Aplication architecture//////////////
 class App {
   #map;
 
